672629d42582ee80ed360dcb44f1767680703e79
Sergey Lukjanov <me@slukjanov.name>
2015-11-22 18:18:52 +0300

    Use currencyzen as target output dir

--- tools/aggregate_wrapper.sh
+++ tools/aggregate_wrapper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-flock -n /var/run/salaryzen/aggregator.lock timeout -k 2m 3m /opt/SalaryZenAggregator/tools/aggregate.sh -o /etc/public_files/salaryzen/data.json 2>&1 | tee -a  /var/log/salaryzen/aggregate.log
+flock -n /var/run/salaryzen/aggregator.lock timeout -k 2m 3m /opt/SalaryZenAggregator/tools/aggregate.sh -o /etc/public_files/currencyzen/data.json 2>&1 | tee -a  /var/log/salaryzen/aggregate.log
